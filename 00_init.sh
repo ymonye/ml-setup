51d42a3c0801b47f885e678713c21f1cd3786875
Yannick Monye <yannick@ledgerql.com>
2025-08-06 22:26:35 +0000

    Initialize NodeJS & Claude Code.

---
+++ 00_init.sh
@@ -0,0 +1,21 @@
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+
+# Download and install Node.js:
+nvm install 22
+
+# Verify the Node.js version:
+node -v # Should print "v22.18.0".
+nvm current # Should print "v22.18.0".
+
+# Verify npm version:
+npm -v # Should print "10.9.3".
+
+# Install Claude Code:
+npm install -g @anthropic-ai/claude-code
+
+# Load Claude Code:
+claude
